fe16efcbf14b0b17ac3207f36011da9cd67f28f9
strong-Ting <s8527502987@gmail.com>
2017-08-05 05:08:14 +0800

    print looooooooooooop and now AM5:06 FUCK UP,I need to commit two small program

---
+++ loop.py
@@ -0,0 +1,10 @@
+def print_for_n(sentence,n):
+    for i in range(n):
+        print(sentence)
+
+def print_while_n(sentence,n):
+    while n != 0:
+        print(sentence)
+        n = n-1
+
+print_while_n("lol",10) 
