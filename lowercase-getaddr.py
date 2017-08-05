66f2e999d43d2fb299568430a17defcae6556946
strong-Ting <s8527502987@gmail.com>
2017-08-05 21:32:45 +0800

    Use getaddr()

---
+++ lowercase-getaddr.py
@@ -0,0 +1,38 @@
+class a:
+    def lowercase0(s):
+        for c in s:
+            if c.islower():
+                return True
+            else:   
+                return False
+
+
+    def lowercase1(s):
+        for c in s:
+            if c.islower():
+                return True
+            else:
+                return False
+
+    def lowercase2(s):
+        for c in s:
+            flag = c.islower()
+        return flag
+
+    def lowercase3(s):
+        flag = False
+        for c in s:
+            flag = flag or c.islower() 
+        return flag
+
+    def lowercase4(s):
+        for c in s:
+            if not c.islower():
+                return False
+        return True
+
+s = input('str:')
+for i in range(5):
+    fuction = getattr(a,"lowercase"+str(i))
+    print(fuction(s))
+
